485135065148051287a4d1fa1b60425109ec94ab
vnarvaezt <vnarvaezt@gmail.com>
2022-09-22 17:16:06 +0200

    preprocessing modify

--- src/preprocessing/preprocessing_main.py
+++ src/preprocessing/preprocessing_main.py
@@ -7,27 +7,25 @@ import re
 from unidecode import unidecode
 
 
-def preprocessing(df, verbose=False):
+def preprocessing(df, verbose=True):
 
     # lower case tweets
     df["text"] = df["text"].str.lower()
-
+    # store all hashtags into a column
     df["hashtags"] = df["text"].apply(lambda y: [x.group() for x in re.finditer(r'#[a-zA-Z]+', y)])
     # Find all @
     df["arrobas"] = df["text"].apply(lambda y: [x.group() for x in re.finditer(r'@[a-zA-Z]+', y)])
-
     # replace https links with "link"
     regex_http = r'([\w+]+\:\/\/)?([\w\d-]+\.)*[\w-]+[\.\:]\w+([\/\?\=\&\#\.]?[\w-]+)*\/?'
-    df["text_clean"] = [re.sub(regex_http, '<link>', doc) for doc in df["text"]]
-
+    #df["text_clean"] = [re.sub(regex_http, '<link>', doc) for doc in df["text"]]
+    df["text_clean"] = [re.sub(regex_http, '', doc) for doc in df["text"]]
     # replace years with "annee"
-    df["text_clean"] = [re.sub(r"[0-9]{4}", "<annee>", doc) for doc in df["text_clean"]]
-
+    #df["text_clean"] = [re.sub(r"[0-9]{4}", "<annee>", doc) for doc in df["text_clean"]]
+    df["text_clean"] = [re.sub(r"[0-9]{4}", "", doc) for doc in df["text_clean"]]
     # delete accents and emojis
     df["text_clean"] = [unidecode(doc) for doc in df["text_clean"]]
-
     # replace all non alphabetic chars with an space
-    df["text_clean"] = [re.sub(r"[^a-z<>]+", ' ', doc) for doc in df["text_clean"]]
+    df["text_clean"] = [re.sub(r"[^a-z]+", ' ', doc) for doc in df["text_clean"]]
 
     # detect language
     tweet_lg = []
@@ -37,6 +35,11 @@ def preprocessing(df, verbose=False):
 
     # store language into a column
     df["language"] = tweet_lg
+    if verbose:
+        other_lang = df[df["language"] != "fr"]
+        nb_other_lang = other_lang.groupby("language")["tweet_id"].nunique()
+        print(f"Nb of tweets detected in other language {nb_other_lang}")
+        print(other_lang[["text_clean", "language"]].iloc[:10])
 
     # filter only french tweets
     is_french = "fr"
@@ -48,6 +51,10 @@ def preprocessing(df, verbose=False):
     df["text_clean"] = [' '.join([word for word in doc.split() if word not in french_stopwords_list]) for doc in
                         df["text_clean"]]
 
+    # filter words with less than 2 syllables
+    df["text_clean"] = [' '.join([word for word in doc.split() if len(word) > 1]) for doc in
+                        df["text_clean"]]
+
     # stem text
     stemmer = SnowballStemmer('french')
     df["text_stem"] = [" ".join([stemmer.stem(word) for word in doc.split()]) for doc in df["text_clean"]]
@@ -56,5 +63,4 @@ def preprocessing(df, verbose=False):
     lemmatizer = FrenchLefffLemmatizer()
     df["text_lemma"] = [" ".join([lemmatizer.lemmatize(word) for word in doc.split()]) for doc in df["text_clean"]]
 
-    #todo: filter 1 syllables
     return df
