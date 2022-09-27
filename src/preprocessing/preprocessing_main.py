092f612c0668dd6c7a4e50284cb0f2c54021dba0
vnarvaezt <valentina.narvaez-trujillo@etu.univ-paris1.fr>
2022-09-27 13:15:34 +0000

    graphs

--- src/preprocessing/preprocessing_main.py
+++ src/preprocessing/preprocessing_main.py
@@ -18,15 +18,15 @@ def preprocessing(df, verbose=True):
     df["arrobas"] = df["text"].apply(lambda y: [x.group() for x in re.finditer(r'@[a-zA-Z]+', y)])
     # replace https links with "link"
     regex_http = r'([\w+]+\:\/\/)?([\w\d-]+\.)*[\w-]+[\.\:]\w+([\/\?\=\&\#\.]?[\w-]+)*\/?'
-    #df["text_clean"] = [re.sub(regex_http, '<link>', doc) for doc in df["text"]]
+    # df["text_clean"] = [re.sub(regex_http, '<link>', doc) for doc in df["text"]]
     df["text_clean"] = [re.sub(regex_http, '', doc) for doc in df["text"]]
     # replace years with "annee"
-    #df["text_clean"] = [re.sub(r"[0-9]{4}", "<annee>", doc) for doc in df["text_clean"]]
-    df["text_clean"] = [re.sub(r"[0-9]{4}", "", doc) for doc in df["text_clean"]]
+    # df["text_clean"] = [re.sub(r"[0-9]{4}", "<annee>", doc) for doc in df["text_clean"]]
+    df["text_clean"] = df["text_clean"].apply(lambda x: re.sub(r"[0-9]{4}", "", x))
     # delete accents and emojis
-    df["text_clean"] = [unidecode(doc) for doc in df["text_clean"]]
+    df["text_clean"] = df["text_clean"].apply(lambda x: unidecode(x))
     # replace all non alphabetic chars with an space
-    df["text_clean"] = [re.sub(r"[^a-z]+", ' ', doc) for doc in df["text_clean"]]
+    df["text_clean"] = df["text_clean"].apply(lambda x: re.sub(r"[^a-z]+", " ", x))
 
     # detect language
     tweet_lg = []
@@ -49,24 +49,37 @@ def preprocessing(df, verbose=True):
     # recover french stopwords
     nltk.download('stopwords')
     french_stopwords_list = stopwords.words('french')
-    df["text_clean"] = [' '.join([word for word in doc.split() if word not in french_stopwords_list]) for doc in
-                        df["text_clean"]]
+    other_stopwords = ["voila", "cette"]
+    all_stopwords_list = french_stopwords_list + other_stopwords
 
+    df["text_clean"] = df['text_clean'].apply(
+        lambda x: ' '.join([word for word in x.split() if word not in (
+            all_stopwords_list)])
+            )
     # filter words with less than 2 syllables
-    df["text_clean"] = [' '.join([word for word in doc.split() if len(word) > 1]) for doc in
-                        df["text_clean"]]
+    df["text_clean"] = df['text_clean'].apply(lambda x: " ".join([
+        word for word in x.split() if len(word) > 1]))
 
     # stem text
     stemmer = SnowballStemmer('french')
-    df["text_stem"] = [" ".join([stemmer.stem(word) for word in doc.split()]) for doc in df["text_clean"]]
+    df["text_stem"] = df['text_clean'].apply(lambda x: " ".join([
+        stemmer.stem(word) for word in x.split()]))
+
+    df["text_stem_bigrame"] = df['text_stem'].apply(lambda x: " ".join([
+        word for word in generate_ngrams(x, 2) if len(word) > 1]))
+
+    df["text_stem_trigrame"] = df['text_stem'].apply(lambda x: " ".join([
+        word for word in generate_ngrams(x, 3) if len(word) > 1]))
 
     # lemmatize text
     lemmatizer = FrenchLefffLemmatizer()
-    df["text_lemma"] = [" ".join([lemmatizer.lemmatize(word) for word in doc.split()]) for doc in df["text_clean"]]
+    df["text_lemma"] = df['text_clean'].apply(lambda x: " ".join([
+        lemmatizer.lemmatize(word) for word in x.split()]))
 
-    df["text_lemma_bigrame"] = [' '.join([word for word in generate_ngrams(doc, 2) if len(word) > 1]) for doc in
-                                  df["text_clean"]]
-    df["text_lemma_trigrame"] = [' '.join([word for word in generate_ngrams(doc, 3) if len(word) > 1]) for doc in
-                                   df["text_clean"]]
+    df["text_lemma_bigrame"] = df['text_lemma'].apply(lambda x: " ".join([
+        word for word in generate_ngrams(x, 2) if len(word) > 1]))
+
+    df["text_lemma_trigrame"] = df['text_lemma'].apply(lambda x: " ".join([
+        word for word in generate_ngrams(x, 3) if len(word) > 1]))
 
     return df
